baabb5e5f6bda4f2bd0d4446fa53437044ac6b48
Nichkamon Promputta <fainichkamon@gmail.com>
2020-08-31 16:55:08 +0700

    up controll

--- Homework2/Controllers/Order.cs
+++ Homework2/Controllers/Order.cs
@@ -15,24 +15,43 @@ namespace Homework2.Controllers
             var fullname = HttpContext.Request.Form["name"].ToString();
             ViewBag.name = fullname;
 
-            var listgame = new List<ListData>();
-            listgame.Add(new ListData { game = "Far Cry 5", price = 620 });
-            listgame.Add(new ListData { game="Gta 5",price= 550});
-            listgame.Add(new ListData { game="Dota 2",price= 360});
-            listgame.Add(new ListData { game="Valorant",price= 130});
-            listgame.Add(new ListData { game="Monter Hunter World",price= 480});
-            listgame.Add(new ListData { game="Assassin's Creed",price= 510});
-            listgame.Add(new ListData { game="Tom Clancys",price= 610});
-            listgame.Add(new ListData { game="Doom",price= 220});
-            listgame.Add(new ListData { game="Division",price= 145});
-            listgame.Add(new ListData { game="Bettlefield",price= 310});
-            listgame.Add(new ListData { game="Just Cause",price= 210});
-            listgame.Add(new ListData { game="Fall Guys",price= 510});
-            listgame.Add(new ListData { game="Watch Dogs2",price= 650});
-            listgame.Add(new ListData { game="Over Wacth",price= 430});
-            listgame.Add(new ListData { game="Devil Maycry5",price= 220});
+            var game = ("Far Cry 5", "Gta 5", "Dota 2", "Valorant", "Monter Hunter World", "Assassin's Creed",
+                "Tom Clancys", "Doom", "Division", "Bettlefield", "Just Cause", "Fall Guys", "Watch Dogs2",
+                "Over Wacth", "Devil Maycry5");
+            ViewData["game1"] = game.Item1;
+            ViewData["game2"] = game.Item2;
+            ViewData["game3"] = game.Item3;
+            ViewData["game4"] = game.Item4;
+            ViewData["game5"] = game.Item5;
+            ViewData["game6"] = game.Item6;
+            ViewData["game7"] = game.Item7;
+            ViewData["game8"] = game.Item8;
+            ViewData["game9"] = game.Item9;
+            ViewData["game10"] = game.Item10;
+            ViewData["game11"] = game.Item11;
+            ViewData["game12"] = game.Item12;
+            ViewData["game13"] = game.Item13;
+            ViewData["game14"] = game.Item14;
+            ViewData["game15"] = game.Item15;
 
-            return View(listgame);
+            var price = ("620", "550", "360", "130", "480", "510", "610", "220", "145", "310", "210", "510", "650", "430", "220");
+            ViewData["price1"] = price.Item1;
+            ViewData["price2"] = price.Item2;
+            ViewData["price3"] = price.Item3;
+            ViewData["price4"] = price.Item4;
+            ViewData["price5"] = price.Item5;
+            ViewData["price6"] = price.Item6;
+            ViewData["price7"] = price.Item7;
+            ViewData["price8"] = price.Item8;
+            ViewData["price9"] = price.Item9;
+            ViewData["price10"] = price.Item10;
+            ViewData["price11"] = price.Item11;
+            ViewData["price12"] = price.Item12;
+            ViewData["price13"] = price.Item13;
+            ViewData["price14"] = price.Item14;
+            ViewData["price15"] = price.Item15;
+
+            return View();
         }
 
         public IActionResult Login()
